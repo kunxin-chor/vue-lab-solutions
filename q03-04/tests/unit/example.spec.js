7df8655c7ac9fc9c50a218bc908e19453f6be258
kunxin-chor <chorkunxin@yahoo.com>
2022-07-25 07:33:54 +0000

    Added test case

--- q03-04/tests/unit/example.spec.js
+++ q03-04/tests/unit/example.spec.js
@@ -2,16 +2,16 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 describe('App.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders props.msg when passed', async () => {
     const wrapper = shallowMount(App)
     const buttons = wrapper.findAll("button");
     const div = wrapper.findAll('div').at(1);
 
     await buttons.at(0).trigger('click');
-    expect(div.element.style.height).toBe(60);
+    expect(div.element.style.height).toBe('60px');
     
     await buttons.at(1).trigger('click');
-    expect(div.element.style.width).toBe(60);
+    expect(div.element.style.width).toBe('60px');
 
     await buttons.at(2).trigger('click');
     expect(div.element.style.backgroundColor).toBe('red');
@@ -21,8 +21,8 @@ describe('App.vue', () => {
     
     await buttons.at(4).trigger('click');
     expect(div.element.style.backgroundColor).toBe('gray');
-    expect(div.element.style.width).toBe(50);
-    expect(div.element.style.width).toBe(50);        
+    expect(div.element.style.width).toBe('50px');
+    expect(div.element.style.width).toBe('50px');        
 
   })
 })
