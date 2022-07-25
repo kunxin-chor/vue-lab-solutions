0f188cd3533dde4d4f53366cd90f6561770579ab
kunxin-chor <chorkunxin@yahoo.com>
2022-07-25 08:45:19 +0000

    Added averagenumber component

--- q04-02/tests/unit/example.spec.js
+++ q04-02/tests/unit/example.spec.js
@@ -1,8 +1,8 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+describe('App.vue', () => {
+  it('should give average result', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
