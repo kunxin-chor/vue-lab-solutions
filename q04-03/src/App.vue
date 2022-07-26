4011f1e060e5902ebc1d2f817b0f78738e63b475
kunxin-chor <chorkunxin@yahoo.com>
2022-07-26 06:50:09 +0000

    Added solutions

--- q04-03/src/App.vue
+++ q04-03/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <traffic-light></traffic-light>
-    <button>Go</button>
-    <button>Wait</button>
-    <button>Stop</button>
+    <traffic-light v-bind:signal='status'></traffic-light>
+    <button v-on:click="go">Go</button>
+    <button v-on:click="wait">Wait</button>
+    <button v-on:click="stop">Stop</button>
   </div>
 </template>
 
@@ -12,12 +12,24 @@ import TrafficLight from '@/TrafficLight.vue'
 
 export default {
   name: 'App',
+  data:function(){
+    return {
+      status:''
+    }
+  },
+  methods:{
+    go:function(){
+      this.status = 'go'
+    },
+    wait:function(){
+      this.status='wait'
+    },
+    stop:function(){
+      this.status='stop'
+    }
+  },
   components: {
     TrafficLight
   }
 }
 </script>
-
-
-
-    TrafficLight
