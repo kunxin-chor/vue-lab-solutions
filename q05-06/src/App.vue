567f908e6d9dc3eb55d72d576238238df108ef0b
kunxin-chor <chorkunxin@yahoo.com>
2022-08-16 08:30:51 +0000

    Added solutions

--- q05-06/src/App.vue
+++ q05-06/src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <input type="checkbox"/><label>A</label>
-    <input type="checkbox"/><label>B</label>
-    <input type="checkbox"/><label>C</label>
-    <input type="checkbox"/><label>D</label>
-    <input type="checkbox"/><label>E</label>
-    <input type="checkbox"/><label>F</label>
-    <div id="result"></div>
+    <input type="checkbox" value="a" v-model="selectedCheck" /><label>A</label>
+    <input type="checkbox" value="b" v-model="selectedCheck" /><label>B</label>
+    <input type="checkbox" value="c" v-model="selectedCheck" /><label>C</label>
+    <input type="checkbox" value="d" v-model="selectedCheck" /><label>D</label>
+    <input type="checkbox" value="e" v-model="selectedCheck" /><label>E</label>
+    <input type="checkbox" value="f" v-model="selectedCheck" /><label>F</label>
+    <div id="result">{{ selectedCheck.length }}</div>
   </div>
-
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data:function(){
+  name: "App",
+  data: function () {
     return {
-    
-    }
+      selectedCheck: [],
+    };
   },
-  components: {
-   
-  }
-}
+  components: {},
+};
 </script>
 
 
