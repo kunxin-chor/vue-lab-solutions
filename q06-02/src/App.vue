2c4d3ccd7dc0405f77e7969b2d0b6b5175554885
kunxin-chor <chorkunxin@yahoo.com>
2022-08-16 10:50:46 +0000

    Added automated testing

--- q06-02/src/App.vue
+++ q06-02/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <input type="text"/>
-    <div id="count"></div>
+    <input type="text" v-model="message"/>
+    <div id="count">{{count}}</div>
   </div>
 </template>
 
@@ -11,12 +11,12 @@ export default {
   name: 'App',
   data:function(){
     return {
-     
+      message:""
     }
   },
   computed:{
     count() {
-
+      return this.message.length;
     }
   },
   components: {
